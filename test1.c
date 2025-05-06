c84b36e6d7fb1d94f2c358494b9cc33555acefe1
Akshaygowdadevaraj <akshaygowda@s.amity.edu>
2025-05-06 11:58:25 +0530

    test code1

---
+++ test1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int dividend, divisor, quotient, remainder;
+
+   
+    printf("Enter dividend: ");
+    scanf("%d", &dividend);
+    printf("Enter divisor: ");
+
